0d2b4790515a68a6353ab73401f8274cc3d4a5ef
pat-s <patrick.schratz@gmail.com>
2021-11-22 22:59:32 +0100

    rename to Formula

--- Formula/rcli.rb
+++ Formula/rcli.rb
@@ -1,5 +1,5 @@
 class Rcli < Formula
-  desc ""
+  desc "Command Line tool to install and switch between R versions"
   homepage "https://github.com/pat-s/rcli"
   url "file:///Users/pjs/git/rcli/bin/rcli-v0.1.0.tar.gz"
   sha256 "e7efa5859e9c91a65dfdcf2d8122f95c48acf1d3679439cff0a26226c94fb265"
