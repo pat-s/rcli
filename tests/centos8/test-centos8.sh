f28a43e282e7b84e6460c7a1a05eeffcbcb0c486
pat-s <patrick.schratz@gmail.com>
2021-12-15 22:36:35 +0100

    dev

--- tests/centos8/test-centos8.sh
+++ tests/centos8/test-centos8.sh
@@ -1,8 +1,8 @@
 mkdir /tmp/test-results
-rcli install 4.1.2 >>/tmp/test-results/out.txt
+./rcli.sh install 4.1.2 >>/tmp/test-results/out.txt
 R -q -s -e "R.version.string" >/tmp/test-results/out.txt
-rcli install 3.6.3 >>/tmp/test-results/out.txt
+./rcli.sh install 3.6.3 >>/tmp/test-results/out.txt
 R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
-rcli switch 4.1.2 >>/tmp/test-results/out.txt
+./rcli.sh switch 4.1.2 >>/tmp/test-results/out.txt
 R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
 if [[ $(diff tests/centos8/test-out.txt /tmp/test-results/out.txt) == "" ]]; then exit 0; else diff --unified tests/centos8/test-out.txt /tmp/test-results/out.txt && exit 1; fi
