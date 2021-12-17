96052b98472deeda2e996422a075b258a1d69326
pat-s <patrick.schratz@gmail.com>
2021-12-17 09:42:51 +0100

    fix fedora test

--- tests/fedora-latest/test-fedora.sh
+++ tests/fedora-latest/test-fedora.sh
@@ -1,5 +1,5 @@
 mkdir -p /tmp/test-results
 
-./rcli.sh install 4.1.2 >>/tmp/test-results/out
-R -q -s -e "R.version.string" >/tmp/test-results/out >>/tmp/test-results/out
-if [[ $(diff tests/fedora/test-out.txt /tmp/test-results/out) == "" ]]; then exit 0; else diff --unified tests/fedora/test-out.txt /tmp/test-results/out && exit 1; fi
+./rcli.sh install 4.1.2 >>/tmp/test-results/out.txt
+R -q -s -e "R.version.string" >/tmp/test-results/out >>/tmp/test-results/out.txt
+if [[ $(diff tests/fedora/test-out.txt /tmp/test-results/out) == "" ]]; then exit 0; else diff --unified tests/fedora/test-out.txt /tmp/test-results/out.txt && exit 1; fi
