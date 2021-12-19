a04e32578614ffe9e3f4a4ceb9a40a423559f927
pat-s <patrick.schratz@gmail.com>
2021-12-19 10:58:54 +0100

    [ci-skip] centos7 nightly adaptions

--- tests/centos7/test-centos7.sh
+++ tests/centos7/test-centos7.sh
@@ -5,4 +5,4 @@ R -q -s -e "R.version.string" >/tmp/test-results/out.txt
 R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
 ./rcli.sh switch 4.1.2 >>/tmp/test-results/out.txt
 R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
-if [[ $(diff tests/centos8/test-out.txt /tmp/test-results/out.txt) == "" ]]; then exit 0; else diff --unified tests/centos8/test-out.txt /tmp/test-results/out.txt && exit 1; fi
+if [[ $(diff tests/centos7/test-out.txt /tmp/test-results/out.txt) == "" ]]; then exit 0; else diff --unified tests/centos7/test-out.txt /tmp/test-results/out.txt && exit 1; fi
