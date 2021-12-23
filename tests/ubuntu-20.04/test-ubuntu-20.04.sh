a02e13302ad143bc3d44458e543080aeef81438d
pat-s <patrick.schratz@gmail.com>
2021-12-23 16:03:27 +0100

    add test

--- tests/ubuntu-20.04/test-ubuntu-20.04.sh
+++ tests/ubuntu-20.04/test-ubuntu-20.04.sh
@@ -1,9 +1,10 @@
 mkdir -p /tmp/test-results
 chmod +x /usr/local/bin/./rcli.sh
-./rcli.sh install 4.1.2 >>/tmp/test-results/out
-R -q -s -e "R.version.string" >/tmp/test-results/out
-./rcli.sh install 3.6.3 >>/tmp/test-results/out
-R -q -s -e "R.version.string" >>/tmp/test-results/out
-./rcli.sh switch 4.1.2 >>/tmp/test-results/out
-R -q -s -e "R.version.string" >>/tmp/test-results/out
-if [[ $(diff tests/ubuntu-20.04/test-out.txt /tmp/test-results/out) == "" ]]; then exit 0; else exit 1; fi
+./rcli.sh install >>/tmp/test-results/out.txt
+./rcli.sh install 4.1.2 >>/tmp/test-results/out.txt
+R -q -s -e "R.version.string" >/tmp/test-results/out.txt
+./rcli.sh install 3.6.3 >>/tmp/test-results/out.txt
+R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
+./rcli.sh switch 4.1.2 >>/tmp/test-results/out.txt
+R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
+if [[ $(diff tests/ubuntu-20.04/test-out.txt /tmp/test-results/out.txt) == "" ]]; then exit 0; else exit 1; fi
