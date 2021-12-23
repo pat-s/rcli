57b2fcf966d91d30890858e4c379699219719a02
pat-s <patrick.schratz@gmail.com>
2021-12-23 21:44:03 +0100

    ubuntu test fixes

--- tests/ubuntu-20.04/test-ubuntu-20.04.sh
+++ tests/ubuntu-20.04/test-ubuntu-20.04.sh
@@ -2,7 +2,7 @@ mkdir -p /tmp/test-results
 
 ./rcli.sh install >>/tmp/test-results/out.txt
 ./rcli.sh install 4.1.2 >>/tmp/test-results/out.txt
-R -q -s -e "R.version.string" >/tmp/test-results/out.txt
+R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
 ./rcli.sh install 3.6.3 >>/tmp/test-results/out.txt
 R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
 ./rcli.sh switch 4.1.2 >>/tmp/test-results/out.txt
