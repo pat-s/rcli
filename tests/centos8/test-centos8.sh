8dbf74ee96b81b65b0e7a7a9f0134d90a5b6cb16
pat-s <patrick.schratz@gmail.com>
2021-12-26 13:44:22 +0100

    ups

--- tests/centos8/test-centos8.sh
+++ tests/centos8/test-centos8.sh
@@ -4,7 +4,7 @@ set -x
 mkdir /tmp/test-results
 
 ./rcli.sh install 4.1.1 >>/tmp/test-results/out.txt
-R -q -s -e "R.version.string" >/tmp/test-results/out.txt
+R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
 ./rcli.sh install 3.6.3 >>/tmp/test-results/out.txt
 R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
 ./rcli.sh switch 4.1.1 >>/tmp/test-results/out.txt
