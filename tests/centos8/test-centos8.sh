947f9b9d45efd9418bfb6e0802892d03681bd92e
Patrick Schratz <patrick.schratz@gmail.com>
2021-12-26 15:51:45 +0100

    Update centos tests (#15)

--- tests/centos8/test-centos8.sh
+++ tests/centos8/test-centos8.sh
@@ -1,8 +1,13 @@
+set -e
+set -x
+
 mkdir /tmp/test-results
-./rcli.sh install 4.1.2 >>/tmp/test-results/out.txt
-R -q -s -e "R.version.string" >/tmp/test-results/out.txt
+
+./rcli.sh install 4.1.1 >>/tmp/test-results/out.txt
+R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
 ./rcli.sh install 3.6.3 >>/tmp/test-results/out.txt
 R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
-./rcli.sh switch 4.1.2 >>/tmp/test-results/out.txt
+./rcli.sh switch 4.1.1 >>/tmp/test-results/out.txt
 R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
+
 if [[ $(diff tests/centos8/test-out.txt /tmp/test-results/out.txt) == "" ]]; then exit 0; else diff --unified tests/centos8/test-out.txt /tmp/test-results/out.txt && exit 1; fi
