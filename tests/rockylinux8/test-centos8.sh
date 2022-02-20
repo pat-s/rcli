c3a9e268900088da83de591415bd971465b1c624
pat-s <patrick.schratz@gmail.com>
2022-02-20 16:53:54 +0100

    more rockylinux replacements

--- tests/rockylinux8/test-centos8.sh
+++ tests/rockylinux8/test-centos8.sh
@@ -10,4 +10,4 @@ R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
 ./rcli.sh switch 4.1.1 >>/tmp/test-results/out.txt
 R -q -s -e "R.version.string" >>/tmp/test-results/out.txt
 
-if [[ $(diff tests/centos8/test-out.txt /tmp/test-results/out.txt) == "" ]]; then exit 0; else diff --unified tests/centos8/test-out.txt /tmp/test-results/out.txt && exit 1; fi
+if [[ $(diff tests/rockylinux8/test-out.txt /tmp/test-results/out.txt) == "" ]]; then exit 0; else diff --unified tests/rockylinux8/test-out.txt /tmp/test-results/out.txt && exit 1; fi
